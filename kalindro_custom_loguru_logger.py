fde64a0402a5c2010ddb9453bc3364b8107ec673
kalindro <25466220+Kalindro@users.noreply.github.com.>
2024-03-28 21:48:19 +0100

    Origin

--- kalindro_custom_loguru_logger.py
+++ kalindro_custom_loguru_logger.py
@@ -1,11 +1,11 @@
+# logger_config.py
+
 import os
 import sys
 import warnings
-from typing import Any
 
 from loguru import logger
 
-# Define default configuration
 default_level = 'INFO'
 custom_format = ("<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
                  "<level>{level: <9}</level> | "
@@ -13,41 +13,34 @@ custom_format = ("<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
                  "<blue>{function}</blue> | "
                  "<magenta>{file}:{line}</magenta>")
 
-
-class MyLogger:
-    def __init__(self, logger):
-        self._logger = logger
-        # Default configuration applied upon initialization
-        self.my_config()
-
-    def my_config(self, log_dir: str = None, level: str = default_level, format: str = custom_format):
-        """
-        Custom configuration method for the logger. It can be called to reconfigure logging.
-        Parameters:
-            log_dir (str): Optional; the directory where logs should be saved.
-            level (str): Logging level as a string. Defaults to 'INFO'.
-            format (str): Log message format.
-        """
-        self._logger.remove()  # Clear existing handlers
-
-        # Configure standard output
-        self._logger.add(sys.stderr, format=format, level=level)
-
-        # Configure file logging if a directory is specified
-        if log_dir:
-            if not os.path.exists(log_dir):
-                warnings.warn(f"Log directory '{log_dir}' does not exist. Logging will proceed to stderr only.")
-            else:
-                file_path = os.path.join(log_dir, "loguru_logs.log")
-                self._logger.add(file_path, format=format, level=level, rotation="10 MB")
-
-    def __getattr__(self, item: str) -> Any:
-        """
-        Delegate attribute access to the underlying loguru logger instance.
-        This allows the MyLogger instance to be used just like a loguru logger.
-        """
-        return getattr(self._logger, item)
+LEVELS = {
+    'INFO': 'INFO',
+    'DEBUG': 'DEBUG',
+    'ERROR': 'ERROR'
+}
 
 
-# Instantiate MyLogger with the loguru logger
-default_logger = MyLogger(logger)
+def setup_logging(level=default_level, log_dir=None):
+    """
+    Reconfigure the loguru logger based on the given log directory and level.
+    This function is intended to be used for optional reconfiguration.
+    """
+
+    logger.remove()  # Clear existing handlers
+    if level:
+        if level not in LEVELS.values():
+            raise ValueError(f"Invalid logging level: {level}. Available levels are: {list(self.LEVELS.keys())}")
+        logger.add(sink=sys.stderr, level=level, format=custom_format)
+
+    if log_dir:
+        if not os.path.exists(log_dir):
+            warnings.warn(f"Provided log directory '{log_dir}' does not exist. Logging will proceed to stderr only.")
+        else:
+            logs_path = os.path.join(log_dir, "loguru_logs.log")
+            logger.add(sink=logs_path, level="ERROR", format=custom_format)
+
+
+setup_logging()
+
+logger.my_config = setup_logging
+default_logger = logger
--- tesr.py
+++ tesr.py
@@ -2,4 +2,4 @@ from backup_logger import default_logger as log1
 from kalindro_custom_loguru_logger import default_logger as log2
 from backup_new import default_logger as log3
 
-log3.
+log2.
