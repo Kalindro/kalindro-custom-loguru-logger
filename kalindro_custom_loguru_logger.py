7be7c5ec78aabd66762f96631134f56a55d43933
kalindro <25466220+Kalindro@users.noreply.github.com.>
2024-03-28 21:01:47 +0100

    Origin

--- kalindro_custom_loguru_logger.py
+++ kalindro_custom_loguru_logger.py
@@ -1,35 +1,41 @@
+# logger_config.py
+
 import os
 import sys
 import warnings
 
 from loguru import logger
 
+# Define default configuration
+default_log_dir = None
+default_level = 'INFO'
+custom_format = ("<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
+                 "<level>{level: <9}</level> | "
+                 "<level>{message}</level> | "
+                 "<blue>{function}</blue> | "
+                 "<magenta>{file}:{line}</magenta>")
 
-def configure_logger(log_dir=None, level='INFO'):
-    custom_format = ("<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
-                     "<level>{level: <9}</level> | "
-                     "<level>{message}</level> | "
-                     "<blue>{function}</blue> | "
-                     "<magenta>{file}:{line}</magenta>")
 
-    LEVELS = {
-        'INFO': 'INFO',
-        'DEBUG': 'DEBUG',
-        'ERROR': 'ERROR'
-    }
+def setup_logging(log_dir=default_log_dir, level=default_level):
+    """
+    Reconfigure the loguru logger based on the given log directory and level.
+    This function is intended to be used for optional reconfiguration.
+    """
+    logger.remove()  # Clear existing handlers
 
-    if level not in LEVELS.values():
-        raise ValueError(f"Invalid logging level: {level}. Available levels are: {list(LEVELS.keys())}")
+    if log_dir:
+        if not os.path.exists(log_dir):
+            warnings.warn(f"Provided log directory '{log_dir}' does not exist. Logging will proceed to stderr only.")
+        else:
+            logs_path = os.path.join(log_dir, "loguru_logs.log")
+            logger.add(sink=logs_path, level="ERROR", format=custom_format)
 
-    logger.remove()  # Remove any previously added handlers
-    if log_dir and os.path.exists(log_dir):  # Check if log_dir exists
-        logs_path = os.path.join(log_dir, "errors.log")
-        logger.add(sink=logs_path, level=LEVELS['ERROR'], format=custom_format)
-    elif log_dir:
-        warnings.warn(f"Provided log directory '{log_dir}' does not exist. Logging will proceed to stderr only.")
     logger.add(sink=sys.stderr, level=level, format=custom_format)
 
 
-# Dynamically add the configure method to the logger
-logger.configure = configure_logger
+# Automatically configure the logger with default settings on module import
+setup_logging()
+
+# Make the setup_logging function accessible via the logger object
+logger.my_config = setup_logging
 default_logger = logger
