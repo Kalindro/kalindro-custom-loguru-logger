42aeba83ace8517d7ab70f1343dd72751283055a
kalindro <25466220+Kalindro@users.noreply.github.com.>
2024-03-28 21:08:17 +0100

    Origin

--- kalindro_custom_loguru_logger.py
+++ kalindro_custom_loguru_logger.py
@@ -1,35 +1,43 @@
+from loguru import logger
 import os
 import sys
 import warnings
 
-from loguru import logger
 
-
-class LoggerWrapper:
+class MyLogger:
     def __init__(self):
         self._logger = logger
-        self.default_log_dir = None
-        self.default_level = 'INFO'
-        self.custom_format = ("<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
-                              "<level>{level: <9}</level> | "
-                              "<level>{message}</level> | "
-                              "<blue>{function}</blue> | "
-                              "<magenta>{file}:{line}</magenta>")
+        self.default_format = ("<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
+                               "<level>{level: <8}</level> | "
+                               "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
+                               "<level>{message}</level>")
+        # Automatically configure with default settings
         self.setup_logging()
 
     def setup_logging(self, log_dir=None, level='INFO'):
-        self._logger.remove()  # Clear existing handlers
-        if log_dir:
+        """
+        Configures the underlying loguru logger instance with the specified log directory and level.
+        This method can be called multiple times to reconfigure the logger.
+        """
+        # Remove all handlers
+        self._logger.remove()
+
+        # Set format and level for stderr
+        self._logger.add(sys.stderr, format=self.default_format, level=level)
+
+        # Configure file logging if a directory is provided
+        if log_dir is not None:
             if not os.path.exists(log_dir):
                 warnings.warn(f"Log directory '{log_dir}' does not exist. Logging will proceed to stderr only.")
             else:
-                logs_path = os.path.join(log_dir, "loguru_logs.log")
-                self._logger.add(sink=logs_path, level="ERROR", format=self.custom_format)
-        self._logger.add(sink=sys.stderr, level=level, format=self.custom_format)
+                log_file_path = os.path.join(log_dir, "app.log")
+                self._logger.add(log_file_path, format=self.default_format, level=level, rotation="10 MB")
 
-    def __getattr__(self, item):
-        return getattr(self._logger, item)
+    def __getattr__(self, name):
+        """
+        Delegate attribute access to the underlying loguru logger instance.
+        This allows the MyLogger instance to be used just like a loguru logger.
+        """
+        return getattr(self._logger, name)
 
-
-# Use the wrapper
-default_logger = LoggerWrapper()
+default_logger = MyLogger()
