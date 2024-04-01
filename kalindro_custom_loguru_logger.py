c9c932cb8944fb6d9d609a8fe9f9b784e6ff4474
kalindro <25466220+Kalindro@users.noreply.github.com.>
2024-04-01 22:41:26 +0200

    Origin

--- kalindro_custom_loguru_logger.py
+++ kalindro_custom_loguru_logger.py
@@ -19,27 +19,43 @@ _LEVELS = {
 
 
 class ConfiguredLoguru(_Logger):
-    def __init__(self, level=_default_level, log_dir=None):
+    def __init__(self):
         core = _Core()
         super().__init__(core=core, exception=None, depth=0, record=False, lazy=False, colors=False, raw=False, capture=True, patchers=[], extra={})
-        self.my_config(level, log_dir)
+        self.initialize_logger()
 
-    def my_config(self, level=_default_level, log_dir=None):
-        """
-        Reconfigure the logger based on the given log directory and level.
-        """
-        self.remove()  # Clear existing handlers
+    def initialize_logger(self):
+        self.remove()
+        self.set_level(_default_level)
+
+    def set_level(self, level):
         if level not in _LEVELS:
             raise ValueError(f"Invalid logging level: {level}. Available levels are: {list(_LEVELS.keys())}")
-        self.add(sink=sys.stderr, level=level, format=_custom_format)
 
-        if log_dir:
-            if not os.path.exists(log_dir):
-                warnings.warn(f"Provided log directory '{log_dir}' does not exist. Logging will proceed to stderr only.")
-            else:
-                logs_path = os.path.join(log_dir, "loguru_logs.log")
-                self.add(sink=logs_path, level="ERROR", format=_custom_format)
+        with self._core.lock:
+            handlers = self._core.handlers.copy()
+        for handler_id, handler in handlers.items():
+            if "stderr" in str(handler._name):
+                self.remove(handler_id)
+
+        self.add(sink=sys.stderr, level=level.upper(), format=_custom_format)
+
+    def set_logs_dump_location(self, log_dir):
+        if not os.path.exists(log_dir):
+            warnings.warn(f"Provided log directory '{log_dir}' does not exist. Creating now")
+            os.mkdir(log_dir)
+
+        with self._core.lock:
+            handlers = self._core.handlers.copy()
+        for handler_id, handler in handlers.items():
+            if "stderr" not in str(handler._name):
+                self.remove(handler_id)
+
+        logs_path = os.path.join(log_dir, "loguru_logs.log")
+        if os.path.exists(logs_path):
+            os.remove(logs_path)
+
+        self.add(sink=logs_path, level='ERROR', format=_custom_format)
 
 
-# Usage
 default_logger = ConfiguredLoguru()
